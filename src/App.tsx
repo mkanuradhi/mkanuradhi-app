72c0eae813476f1e56c7a2f496aee3d8662ca908
lalankeba <lalankeba@gmail.com>
2024-07-27 11:27:09 +0530

    adding initial react files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className='app'>
+        <h1>Anuradha Ariyaratne</h1>
+        <h3 className='sub-heading'>Sub heading</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
