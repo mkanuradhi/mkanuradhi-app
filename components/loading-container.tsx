3f103025c65801062ca5ddc13f2051032d509f7c
lalankeba <lalankeba@gmail.com>
2025-03-03 16:36:59 +0530

    updating loading component with custom message

--- components/loading-container.tsx
+++ components/loading-container.tsx
@@ -5,13 +5,18 @@ import { Spinner } from 'react-bootstrap';
 
 const baseTPath = 'components.LoadingContainer';
 
-const LoadingContainer = () => {
+interface LoadingContainerProps {
+  message?: string;
+}
+
+const LoadingContainer: React.FC<LoadingContainerProps> = ({ message }) => {
   const t = useTranslations(baseTPath);
+  message = message || t('loading');
 
   return (
     <>
       <div className="d-flex flex-column justify-content-center align-items-center reduced-height-container">
-        <h1 className="m-4">{t('loading')}</h1>
+        <h4 className="m-4">{message}</h4>
         <Spinner animation="border" role="status" className="m-4">
           <span className="visually-hidden">{t('loading')}</span>
         </Spinner>
