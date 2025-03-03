4c5ad8c537387b6c127c910df1aea29fcb706869
lalankeba <lalankeba@gmail.com>
2025-03-03 16:40:49 +0530

    updating loading component with best practice

--- components/loading-container.tsx
+++ components/loading-container.tsx
@@ -11,12 +11,12 @@ interface LoadingContainerProps {
 
 const LoadingContainer: React.FC<LoadingContainerProps> = ({ message }) => {
   const t = useTranslations(baseTPath);
-  message = message || t('loading');
+  const displayMessage = message ?? t('loading');
 
   return (
     <>
       <div className="d-flex flex-column justify-content-center align-items-center reduced-height-container">
-        <h4 className="m-4">{message}</h4>
+        <h4 className="m-4">{displayMessage}</h4>
         <Spinner animation="border" role="status" className="m-4">
           <span className="visually-hidden">{t('loading')}</span>
         </Spinner>
