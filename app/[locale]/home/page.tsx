5b590afad0d666719b9cbb0e7b0b6b14d81c05c7
lalankeba <lalankeba@gmail.com>
2025-11-20 17:08:10 +0530

    update schema.org info for the app

--- app/[locale]/home/page.tsx
+++ app/[locale]/home/page.tsx
@@ -13,6 +13,7 @@ import GlowLink from '@/components/GlowLink';
 import ToolsSkillsDisplayer from '@/components/ToolsSkillsDisplayer';
 import MovingGradientTitle from '@/components/moving-gradient-title';
 import './home.scss';
+import { LANG_EN, LANG_SI } from '@/constants/common-vars';
 
 const baseTPath = 'pages.Home';
 
@@ -43,6 +44,53 @@ interface EducationMessages {
 export async function generateMetadata ({ params }: { params: { locale: string } }) {
   const { locale } = params;
   const t = await getTranslations({ locale, namespace: baseTPath });
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+
+  const personSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    '@id': `${baseUrl}/#person`, // Unique ID for you across all pages
+    name: t('pageTitle'),
+    url: baseUrl,
+    image: `${baseUrl}/images/anuradha.png`,
+    jobTitle: t('subTitle'),
+    description: t('description'),
+    // Skills
+    knowsAbout: [
+      'Meta-heuristic optimization',
+      'Neural Networks',
+      'Swarm Intelligence',
+      'Machine Learning',
+      'Data Science',
+      'Artificial Intelligence',
+      'Natural Language Processing',
+      'Statistical Analysis',
+      'Python Programming',
+      'R Programming',
+      'Nature Inspired Computing',
+      'Nature Inspired Algorithms'
+    ],
+    // Social media links
+    sameAs: [
+      'https://scholar.google.com/citations?user=-O25soMAAAAJ',
+      'https://www.linkedin.com/in/anuradha-ariyaratne-3a406281/',
+      'https://orcid.org/0000-0002-3548-3976',
+      'https://www.researchgate.net/profile/Anuradha-Ariyaratne',
+      'https://www.scopus.com/authid/detail.uri?authorId=57188855115',
+      'https://www.webofscience.com/wos/author/record/NRY-6429-2025',
+      'https://github.com/mkanuradhi'
+    ],
+    // Education
+    alumniOf: {
+      '@type': 'EducationalOrganization',
+      name: t('university')
+    },
+    // Current position
+    worksFor: {
+      '@type': 'Organization',
+      name: t('university')
+    }
+  };
 
   return {
     title: {
@@ -50,10 +98,20 @@ export async function generateMetadata ({ params }: { params: { locale: string }
     },
     description: t('pageDescription'),
     keywords: t('pageKeywords'),
+    alternates: {
+      canonical: `${baseUrl}/${locale}`,
+      languages: {
+        'en': `${baseUrl}/${LANG_EN}`,
+        'si': `${baseUrl}/${LANG_SI}`
+      }
+    },
     openGraph: {
       title: t('pageTitle'),
       description: t('pageDescription'),
-      type: 'website',
+      type: 'profile',
+      locale: locale === LANG_EN ? 'en_US' : 'si_LK',
+      alternateLocale: locale === LANG_EN ? 'si_LK' : 'en_US',
+      url: `${baseUrl}/${locale}`,
       images: [
         {
           url: '/images/mkanuradhi.png',
@@ -68,6 +126,9 @@ export async function generateMetadata ({ params }: { params: { locale: string }
           alt: 'MKA',
         },
       ],
+    },
+    other: {
+      'script:ld+json': JSON.stringify(personSchema)
     }
   };
 };
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,57 @@
 import { getTranslations } from 'next-intl/server';
 import HomePage from './home/page';
+import { LANG_EN, LANG_SI } from '@/constants/common-vars';
 
 export async function generateMetadata ({ params }: { params: { locale: string } }) {
   const { locale } = params;
   const t = await getTranslations({ locale, namespace: 'pages.Home' });
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+
+  const personSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    '@id': `${baseUrl}/#person`, // Unique ID for you across all pages
+    name: t('pageTitle'),
+    url: baseUrl,
+    image: `${baseUrl}/images/anuradha.png`,
+    jobTitle: t('subTitle'),
+    description: t('description'),
+    // Skills
+    knowsAbout: [
+      'Meta-heuristic optimization',
+      'Neural Networks',
+      'Swarm Intelligence',
+      'Machine Learning',
+      'Data Science',
+      'Artificial Intelligence',
+      'Natural Language Processing',
+      'Statistical Analysis',
+      'Python Programming',
+      'R Programming',
+      'Nature Inspired Computing',
+      'Nature Inspired Algorithms'
+    ],
+    // Social media links
+    sameAs: [
+      'https://scholar.google.com/citations?user=-O25soMAAAAJ',
+      'https://www.linkedin.com/in/anuradha-ariyaratne-3a406281/',
+      'https://orcid.org/0000-0002-3548-3976',
+      'https://www.researchgate.net/profile/Anuradha-Ariyaratne',
+      'https://www.scopus.com/authid/detail.uri?authorId=57188855115',
+      'https://www.webofscience.com/wos/author/record/NRY-6429-2025',
+      'https://github.com/mkanuradhi'
+    ],
+    // Education
+    alumniOf: {
+      '@type': 'EducationalOrganization',
+      name: t('university')
+    },
+    // Current position
+    worksFor: {
+      '@type': 'Organization',
+      name: t('university')
+    }
+  };
 
   return {
     title: {
@@ -11,10 +59,20 @@ export async function generateMetadata ({ params }: { params: { locale: string }
     },
     description: t('pageDescription'),
     keywords: t('pageKeywords'),
+    alternates: {
+      canonical: `${baseUrl}/${locale}`,
+      languages: {
+        'en': `${baseUrl}/${LANG_EN}`,
+        'si': `${baseUrl}/${LANG_SI}`
+      }
+    },
     openGraph: {
       title: t('pageTitle'),
       description: t('pageDescription'),
-      type: 'website',
+      type: 'profile',
+      locale: locale === LANG_EN ? 'en_US' : 'si_LK',
+      alternateLocale: locale === LANG_EN ? 'si_LK' : 'en_US',
+      url: `${baseUrl}/${locale}`,
       images: [
         {
           url: '/images/mkanuradhi.png',
@@ -29,6 +87,9 @@ export async function generateMetadata ({ params }: { params: { locale: string }
           alt: 'MKA',
         },
       ],
+    },
+    other: {
+      'script:ld+json': JSON.stringify(personSchema)
     }
   };
 };
