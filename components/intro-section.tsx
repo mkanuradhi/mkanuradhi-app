670d59a7d8718a15a411f5498f680a486740decf
Lalanke Athauda <lalankeba@gmail.com>
2026-07-18 04:32:43 +0530

    Update intro section with layout

--- components/intro-section.tsx
+++ components/intro-section.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Container } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { useTranslations } from "next-intl";
 import "./intro-section.scss";
 
@@ -10,14 +10,14 @@ const IntroSection = () => {
 
   return (
     <section className="intro-section">
-      <Container>
-        <div className="intro-wrapper">
+      <Row>
+        <Col className="intro-wrapper">
           <p className="intro-kicker">{t('title')}</p>
           <p className="intro-text lead">
             {t('introText')}
           </p>
-        </div>
-      </Container>
+        </Col>
+      </Row>
     </section>
   );
 };
